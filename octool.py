6ae1a3d7e34b574a4d638c569bd964ee92ac6755
Josep Damià Carbonell Seguí <josepdcs@ext.inditex.com>
2022-01-21 20:08:36 +0100

    Some pylints

--- octool.py
+++ octool.py
@@ -10,11 +10,11 @@ from columnar import columnar
 
 oc_login_dir = os.getenv("HOME") + "/.oc-tool"
 oc_login_config_file = oc_login_dir + "/config.yml"
-servers = []
+SERVERS = []
 
 
 @click.group(name="OC Tool")
-@click.version_option(version='0.1.2')
+@click.version_option(version='0.1.3')
 def commands():
     pass
 
@@ -28,8 +28,8 @@ def list_servers():
         ('pre.+', lambda text: style(text, fg='yellow')),
         ('pro.+', lambda text: style(text, fg='red')),
     ]
-    if len(servers) > 0:
-        print(columnar(servers, headers, no_borders=True, patterns=patterns))
+    if len(SERVERS) > 0:
+        print(columnar(SERVERS, headers, no_borders=True, patterns=patterns))
     else:
         print("Ops! No servers found. Check your configuration file: {}".format(oc_login_config_file))
 
@@ -41,7 +41,7 @@ def list_servers():
 @click.password_option('--password', '-p', envvar='OC_LOGIN_PASSWORD', confirmation_prompt=False,
                        help='The password for server. Default value is taken from OC_LOGIN_PASSWORD env var.')
 def login_server(server, username, password):
-    """Login against server by using the username"""
+    """Login against server"""
 
     if not username:
         print("Username is mandatory.")
@@ -51,7 +51,7 @@ def login_server(server, username, password):
         print("password is mandatory.")
         exit(1)
 
-    url = list(filter(lambda s: s[0] == server, servers))
+    url = list(filter(lambda s: s[0] == server, SERVERS))
     if len(url) == 0:
         print("Server '{0}' not found".format(server))
         exit(1)
@@ -63,13 +63,13 @@ def login_server(server, username, password):
 
 
 def do_command(command):
-    sub_process = subprocess.Popen(command.split(), universal_newlines=True, stdout=subprocess.PIPE)
-    while True:
-        output = sub_process.stdout.readline()
-        if output:
-            print(output.strip())
-        elif sub_process.poll() is not None:
-            break
+    with subprocess.Popen(command.split(), universal_newlines=True, stdout=subprocess.PIPE) as sub_process:
+        while True:
+            output = sub_process.stdout.readline()
+            if output:
+                print(output.strip())
+            elif sub_process.poll() is not None:
+                break
 
 
 def read_config_file():
@@ -78,13 +78,13 @@ def read_config_file():
         with open(oc_login_config_file, 'w'):
             pass
 
-    global servers
+    global SERVERS
     with open(oc_login_config_file, 'r') as config_file:
         config = yaml.full_load(config_file)
 
     if config:
         for server in config["servers"]:
-            servers.append([server["server"], server["description"], server["api-url"], server["console-url"]])
+            SERVERS.append([server["server"], server["description"], server["api-url"], server["console-url"]])
 
 
 if __name__ == "__main__":
